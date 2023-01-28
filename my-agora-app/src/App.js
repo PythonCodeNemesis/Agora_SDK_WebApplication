e4fc7d263ecad486d7860f19e41132aee8b6ea59
PythonCodeNemesis <durgaminakshmi@gmail.com>
2023-01-28 19:52:30 +0530

    correct

--- my-agora-app/src/App.js
+++ my-agora-app/src/App.js
@@ -5,9 +5,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      channelName: 'python_code_nemesis',
-      appId: '84ad96a5eafe42589c85c01c1aaccede',
-      token: '007eJxTYFh+/9i2xR7HP5nefy+16Ppa6f65ZY0LP+88e+n1L/vHixi/KDBYmCSmWJolmqYmpqWaGJlaWCZbmCYbGCYbJiYmJ6empN5WvprcEMjIwOMylYERCkF8YYaCypKM/Lz45PyU1Pi81NzU4sxiBgYAfG0raA==',
+      channelName: ' ',
+      appId: ' ',
+      token: '',
       videoProfile: '480p_1',
     };
   }
